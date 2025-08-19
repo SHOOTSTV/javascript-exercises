c99d50a79e7dd243997934f6d4f3f9a1a237d477
AxelS <72924802+SHOOTSTV@users.noreply.github.com>
2025-08-19 17:58:52 +0200

    complete 6th exercice repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) return "ERROR";
+  let receivedString = "";
+  for (let i = 0; i < num; i++) {
+    receivedString = receivedString + string;
+  }
+  return receivedString;
 };
 
 // Do not edit below this line
